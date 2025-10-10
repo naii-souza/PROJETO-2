e55e6a89336175d7e64ac64f9f29db8f80f7f93d
naii-souza <naira.nascimento@escola.pr.gov.br>
2025-10-10 11:52:38 -0300

    fazendo

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-let titulo = document.querySelector('h1'); 
-titulo.innerHTML = 'Jogo do número secreto';
+cuuuuuuuuuu
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
 
-function exibirTextoNatela()
-let campo = document.querySelector(tag)
-campo.innerHTML = texto
 
-function verificarChute() {
-    console.log('O botão foi clicado!')
-} 
+
+
+
+
+
+
+
+
+
+
+
