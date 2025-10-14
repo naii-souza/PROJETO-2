65b40be91fd002d6d3ecd98115aa6a2139782a61
naii-souza <naira.nascimento@escola.pr.gov.br>
2025-10-14 11:03:36 -0300

    projeto 2

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
+let numeroSecreto = 8;
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela('h1', 'Jogo do número secreto');
+exibirTextoNaTela('p','Escolha um número entre 1 e 10');
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+
+if (chute == numeroSecreto) {}
+    exibirTextoNaTela('h1','Acertou!')
+    
+
+}
+
+
+
+
+
+
+
+
 
 
 
