e6721c12c6035ea9d3864e8fc63a7f352d67799c
naii-souza <naira.nascimento@escola.pr.gov.br>
2025-10-17 11:51:17 -0300

    fazendo

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let numeroSecreto = 8;
+let tentativas = 1;
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
@@ -8,22 +9,31 @@ function exibirTextoNaTela(tag, texto) {
 exibirTextoNaTela('h1', 'Jogo do número secreto');
 exibirTextoNaTela('p','Escolha um número entre 1 e 10');
 
-function verificarChute() {
-    let chute = document.querySelector('input').value;
+    function verificarChute() {
+let chute = document.querySelector('input').value;
 
-if (chute == numeroSecreto) {}
+if (chute == numeroSecreto) {
     exibirTextoNaTela('h1','Acertou!')
-    exibirTextoNaTela('p','Você descobriu o número secreto!')
+    let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+    let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+      exibirTextoNaTela('p', mensagemTentativas);
 } else {
     if (chute < numeroSecreto) {
         exibirTextoNaTela('p', 'O número secreto é menor')
+    } else {
+        exibirTextoNaTela('p', 'O número secreto é maior')
     }
 
 
+    }
+
+   
 
 
 
-}
+
+
+    }
 
 
 
